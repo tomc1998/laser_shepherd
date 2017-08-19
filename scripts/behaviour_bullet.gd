7b8b470cd7a588ef18ca548fa6dd70758a57e676
Thomas Cheng <thomascheng1998@googlemail.com>
2017-08-19 19:43:05 +0100

    Added bullet lifetime to bullet behaviour script

--- scripts/behaviour_bullet.gd
+++ scripts/behaviour_bullet.gd
@@ -2,12 +2,19 @@ extends KinematicBody2D
 
 var vel = Vector2(0, 0)
 
+var lifetime = 50.0
+
 # Set the velocity of this bullet to the given vector2.
 func set_vel(shoot_vec):
 	vel = shoot_vec
 	set_rot(-atan2(shoot_vec.y, shoot_vec.x))
 
 func _fixed_process(delta):
+	lifetime -= delta;
+	if lifetime <= 0.0:
+		queue_free()
+		return
+
 	move(vel * delta)
 
 func _ready():
